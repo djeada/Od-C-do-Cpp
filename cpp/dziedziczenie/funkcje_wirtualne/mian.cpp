9eb2f5705ac2209aa1155cacb766efd60f887b8c
djeada <addjellouli1@gmail.com>
2019-11-27 14:00:00 +0100

    dodano przyklad do dziedziczenia

---
+++ cpp/dziedziczenie/funkcje_wirtualne/mian.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+When derived class overrides the base class method by redefining the same function, then if client wants to access redefined the method from derived class through a pointer from base class object, then you must define this function in base class as virtual function.
+*/
+
+class parent {
+	public:
+	void Show() {
+		cout << "i'm parent" << endl;
+	}
+	virtual void v_Show(){
+		cout << "i'm parent" << endl;
+	}
+};
+
+class child: public parent {
+	public:
+	void Show() {
+		cout << "i'm child" << endl;
+	}
+	void v_Show(){
+		cout << "i'm child" << endl;
+	}
+};
+ 
+int main(int argc, char* argv[])
+{
+	parent * parent_object_ptr = new child;
+	parent_object_ptr->Show();
+	parent_object_ptr->v_Show();
+	return 0;
+}
+
+
+
+
---
+++ cpp/dziedziczenie/problem_diamentu/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using namespace std;
+
+/* klasa bazowa
+ klasa pochodna
+
+ A
+ |
+ B
+ |
+ C
+
+dziedziczenie wielokrotne
+ A B
+ \/
+ C
+
+problem diamentu (to jest to co chcemy uzyskać)
+  A
+ /\
+ B C
+ \/
+  D
+
+domyślnie mamy:
+A A
+| |
+B C
+\/
+ D
+
+*/
+
+class A {
+  int pole_a;
+
+public:
+  void set_a(int x) { pole_a = x; }
+  void print_a() { cout << "Wartość pola a: " << pole_a << endl; }
+};
+
+class B : virtual public A {
+  int pole_b;
+
+public:
+  void set_b(int x) { pole_b = x; }
+  void print_b() { cout << "Wartość pola b: " << pole_b << endl; }
+};
+
+class C : virtual public A {
+  int pole_c;
+
+public:
+  void set_c(int x) { pole_c = x; }
+  void print_c() { cout << "Wartość pola c: " << pole_c << endl; }
+};
+
+class D : public B, public C {
+  int pole_d;
+
+public:
+  void set_d(int x) { pole_d = x; }
+  void print_d() { cout << "Wartość pola d: " << pole_d << endl; }
+};
+
+int main() {
+
+  cout << "Obiekt klasy A: " << endl;
+  A obiekt_a;
+  obiekt_a.set_a(5);
+  obiekt_a.print_a();
+
+  cout << endl << "Obiekt klasy B: " << endl;
+  B obiekt_b;
+  obiekt_b.set_a(10);
+  obiekt_b.set_b(12);
+  obiekt_b.print_a();
+  obiekt_b.print_b();
+
+  cout << endl << "Obiekt klasy C: " << endl;
+  C obiekt_c;
+  obiekt_c.set_a(7);
+  obiekt_c.set_c(-9);
+  obiekt_c.print_a();
+  obiekt_c.print_c();
+
+  cout << endl << "Obiekt klasy D: " << endl;
+  D obiekt_d;
+  obiekt_d.set_a(2);
+  obiekt_d.set_b(6);
+  obiekt_d.set_c(4);
+  obiekt_d.set_d(-11);
+  obiekt_d.print_a();
+  obiekt_d.print_b();
+  obiekt_d.print_c();
+  obiekt_d.print_d();
+
+  return 0;
+}
