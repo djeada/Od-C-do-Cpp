f831a4527b6c765fa1dabc51ae6ebb0d08465a7f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-03 00:21:15 +0100

    zaktualizuj tresc przykladu [watki wprowadzenie]

--- cpp/przetwarzanie_wspolbiezne/watki_wprowadzenie/main.cpp
+++ cpp/przetwarzanie_wspolbiezne/watki_wprowadzenie/main.cpp
@@ -1,71 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <chrono>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
-#include <mutex>
-#include <numeric>
-#include <sstream>
-#include <string>
 #include <thread>
 #include <vector>
 
-int GetRandom(int max) {
-  srand(time(NULL));
-  return rand() % max;
-}
-
-void ExecuteThread(int id) {
-  // Get current time
-  auto nowTime = std::chrono::system_clock::now();
-
-  // Convert to a time we can output
-  std::time_t sleepTime = std::chrono::system_clock::to_time_t(nowTime);
-
-  // Convert to current time zone
-  tm myLocalTime = *localtime(&sleepTime);
-
-  // Print full time information
-  std::cout << "Thread " << id << " Sleep Time : " << std::ctime(&sleepTime)
-            << "\n";
-
-  // Get separate pieces
-  std::cout << "Month : " << myLocalTime.tm_mon << "\n";
-  std::cout << "Day : " << myLocalTime.tm_mday << "\n";
-  std::cout << "Year : " << myLocalTime.tm_year + 1900 << "\n";
-  std::cout << "Hours : " << myLocalTime.tm_hour << "\n";
-  std::cout << "Minutes : " << myLocalTime.tm_min << "\n";
-  std::cout << "Seconds : " << myLocalTime.tm_sec << "\n\n";
-
-  // Put the thread to sleep for up to 3 seconds
-  std::this_thread::sleep_for(std::chrono::seconds(GetRandom(3)));
-  nowTime = std::chrono::system_clock::now();
-  sleepTime = std::chrono::system_clock::to_time_t(nowTime);
-  std::cout << "Thread " << id << " Awake Time : " << std::ctime(&sleepTime)
-            << "\n";
-}
-
-std::string GetTime() {
-  auto nowTime = std::chrono::system_clock::now();
-  std::time_t sleepTime = std::chrono::system_clock::to_time_t(nowTime);
-  return std::ctime(&sleepTime);
+// Kazdy watek wywola ta funkcje
+void petla(int id) { 
+  for (int i=0; i < 1000; i++)
+      std::cout << "Watek nr: " << id << std::endl;
 }
 
 int main() {
-  // Create a thread and pass a parameter to the function
-  std::thread th1(ExecuteThread, 1);
+  // Odpal watki
+  std::thread watekA(petla, 0);
+  std::thread watekB(petla, 1);
+  std::thread watekC(petla, 2);
+  std::thread watekD(petla, 3);
 
-  // Join the thread to the main thread meaning main waits for this thread
-  // to stop executing before continuing execution  of code in main
-  th1.join();
+  // Zablokuj program przed przejsciem
+  // do nastepnych linii kodu do czasu
+  // az watki nie zakoncza pracy
+  watekA.join();
+  watekB.join();
+  watekC.join();
+  watekD.join();
+  
+  std::cout << "KONIEC" << std::endl;
 
-  std::thread th2(ExecuteThread, 2);
-  th2.join();
-
-  // We will create a pool of threads that
-  // will access a bank account in no particularorder
-
-  system("pause");
   return 0;
 }
