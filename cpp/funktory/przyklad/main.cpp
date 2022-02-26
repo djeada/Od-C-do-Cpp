71febc190fce982c2a96a578414f72c7c0b6a942
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-27 00:45:32 +0100

    dodano przyklad [ c++/ funktory/przyklad ]

---
+++ cpp/funktory/przyklad/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+/*
+Obiekty moga zachowywac sie jak funkcje.
+*/
+
+class PrzykladowaKlasa
+{
+public:
+    const void operator()(const std::string napis)
+    {
+        std::cout << "Otrzymalem napis: " << napis << std::endl;
+    }
+};
+
+// Funktory moga miec normalne konstruktory
+
+class Powieksz
+{
+    int wartosc;
+
+public:
+    Powieksz(int wartosc) : wartosc(wartosc) {}
+    void operator()(int element)
+    {
+        std::cout << element + wartosc << std::endl;
+    }
+};
+
+int main()
+{
+    PrzykladowaKlasa obiekt;
+    obiekt("Witaj swiecie!");
+  
+    std::vector<int> vec = {2, 3, 4, 5};
+    std::for_each(vec.begin(), vec.end(), Powieksz(5)); 
+  
+    return 0;
+}
