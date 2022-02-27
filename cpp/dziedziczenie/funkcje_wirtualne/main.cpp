07115bf20ceb227cd84f5c1e62a04489923cc217
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-27 19:13:37 +0100

    dodano przyklad [ c++/ dziedziczenie/funkcje_wirtualne ]

--- cpp/dziedziczenie/funkcje_wirtualne/main.cpp
+++ cpp/dziedziczenie/funkcje_wirtualne/main.cpp
@@ -27,3 +27,41 @@ int main(int argc, char *argv[]) {
   parent_object_ptr->v_Show();
   return 0;
 }
+
+/*
+
+class Base
+{
+public:
+    virtual void print() const = 0;
+    virtual void printthat() const = 0;
+    virtual void printit() const = 0;
+};
+
+class inhert : public Base
+{
+public:
+    // only virtual keyword for overriding.
+    virtual void print() const {}
+
+    // only override keyword for overriding.
+    void printthat() const override {}
+
+    // using both virtual and override keywords for overriding.
+    virtual void printit() const override {}
+};
+
+When you override a function you don't technically need to write either virtual or override.
+
+The original base class declaration needs the keyword virtual to mark it as virtual.
+
+In the derived class the function is virtual by way of having the ¹same type as the base class function.
+
+However, an override can help avoid bugs by producing a compilation error when the intended override isn't technically an override. 
+For instance, the function type isn't exactly like the base class function. 
+Or that a maintenance of the base class changes that function's type, e.g. adding a defaulted argument.
+
+If some member function vf is declared as virtual in a class Base, and some class Derived, which is derived, directly or indirectly, 
+from Base, has a declaration for member function with the same name parameter type list (but not the return type) cv-qualifiers ref-qualifiers 
+Then this function in the class Derived is also virtual 
+*/
