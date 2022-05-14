e0b395511f5a45620b414e12df749584ca04cb3d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-05-15 00:53:15 +0200

    Create main.c

---
+++ c/wskazniki/preinkrementacja_postinkrementacji/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+int liczba_a = 15;
+int liczba_b = 10;
+
+int *wsk_a = &liczba_a;
+int *wsk_b = &liczba_b;
+
+++*wsk_a; // To samo co ++(*wsk). Moge zmieniac wartosc przez wskaznik.
+*wsk_b++; // To samo co *(wsk++). Moge zmieniac wartosc wskaznika.
+
+printf("Liczba a: %d\n", liczba_a);
+printf("Liczba b: %d\n", liczba_b);
+
+printf("Wartosc na ktora wskazuje wsk_a: %d\n", *wsk_a);
+printf("Wartosc na ktora wskazuje wsk_b: %d\n", *wsk_b);
+
+return 0;
+ }
