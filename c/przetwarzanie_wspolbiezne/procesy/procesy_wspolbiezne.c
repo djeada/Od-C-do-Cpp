2e9a271b1835f5c6fe1c1921a94eea880e9df970
djeada <addjellouli1@gmail.com>
2022-08-22 11:47:00 +0200

    [refaktoryzacja]: popraw przyklad c/przetwarzanie_wspolbiezne/procesy/przetwarzenie_wspolbiezne.c

--- c/przetwarzanie_wspolbiezne/procesy/procesy_wspolbiezne.c
+++ c/przetwarzanie_wspolbiezne/procesy/procesy_wspolbiezne.c
@@ -6,31 +6,33 @@
 // int forkNexec(StrVec argList);
 
 void szeregowo() {
-  char *const parmList[] = {"/bin/sleep", "5", NULL};
+  char *const parametry[] = {"/bin/sleep", "5", NULL};
 
   int pid1 = fork();
-  execv("/bin/sleep", parmList);
+  execv("/bin/sleep", parametry);
   waitpid(pid1, NULL, 0);
 
   int pid2 = fork();
-  execv("/bin/sleep", parmList);
+  execv("/bin/sleep", parametry);
   waitpid(pid2, NULL, 0);
 }
 
 void rownolegle() {
-  char *const parmList[] = {"sleep", "5", NULL};
+  char *const parametry[] = {"sleep", "5", NULL};
 
   int pid1 = fork();
   int pid2 = fork();
 
-  execv("/bin/sleep", parmList);
-  execv("/bin/sleep", parmList);
+  execv("/bin/sleep", parametry);
+  execv("/bin/sleep", parametry);
 
   waitpid(pid1, NULL, 0);
   waitpid(pid2, NULL, 0);
 }
 
 int main() {
+  
+  //szeregowo();
   rownolegle();
 
   return 0;
