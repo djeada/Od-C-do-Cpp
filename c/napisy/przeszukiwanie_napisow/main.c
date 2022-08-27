54b62256ee6f61649f79c27780d17a97e07a7610
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-08-27 23:17:13 +0200

    [rozwin przyklad]: /c/napisy/przeszukiwanie_napisow

--- c/napisy/przeszukiwanie_napisow/main.c
+++ c/napisy/przeszukiwanie_napisow/main.c
@@ -1,1 +1,38 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    
+    // utworz napis
+    char napis[] = "Hello World!";
+
+    // wyswietl napis
+    printf("%s\n", napis);
+
+    // znajdz indeks pierwszego wystapienia znaku 'o'
+    char* indeksWsk = strchr(napis, 'o');
+    printf("%ld\n", indeksWsk - napis);
+
+
+    // co jesli nie ma danego znaku?
+    indeksWsk = strchr(napis, 'x');
+    printf("%ld\n", indeksWsk - napis);
+
+    // lepiej sprawdzic czy nie wskaznik nie jest NULL
+    if (indeksWsk != NULL) {
+        printf("%ld\n", indeksWsk - napis);
+    } else {
+        printf("Nie znaleziono znaku\n");
+    }
+
+    // co jesli chcemy wszystkie wystapienia danego znaku?
+    indeksWsk = strchr(napis, 'o');
+    while (indeksWsk != NULL) {
+        printf("%ld\n", indeksWsk - napis);
+        indeksWsk = strchr(indeksWsk + 1, 'o');
+    }
+  
+    return 0;
+}
