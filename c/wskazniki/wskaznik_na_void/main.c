3867c2c940fe796b8f6228b97d1370218523b584
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-09-22 19:35:25 +0200

    [rozwin przyklad]: c/wskazniki/wskaznik_na_void

--- c/wskazniki/wskaznik_na_void/main.c
+++ c/wskazniki/wskaznik_na_void/main.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    int n = 10;
+    void* arr = malloc(sizeof(int) * n);
 
-  int a = 10;
-  double b = 3.14;
-  char c = 'k';
-
-  void *vp = &a;
-  printf("Wartosc a: %d \n", *(int *)vp);
-
-  vp = &b;
-  printf("Wartosc b: %.2f \n", *(double *)vp);
-
-  vp = &c;
-  printf("Wartosc c: %c \n", *(char *)vp);
-
-  vp = &a;
-  int *p1 = (int *)vp;
-  printf("Wartosc a: %d \n", *p1);
-
-  vp = &b;
-  double *p2 = (double *)vp;
-  printf("Wartosc b: %.2f \n", *p2);
-
-  vp = &c;
-  char *p3 = (char *)vp;
-  printf("Wartosc c: %c \n", *p3);
-
-  vp = p1;
-  printf("Wartosc a: %d \n", *(int *)vp);
-
-  *(int *)vp = *(int *)vp + 10;
-  printf("Wartosc a: %d \n", *(int *)vp);
-
+      for (int i = 0; i < n; i++) {
+        int* p = arr + i*sizeof(int);
+        *p = i;
+      }
+  
+        for (int i = 0; i < n; i++) {
+            int* p = arr + i*sizeof(int);
+            printf("%d \n", *p);
+      }
+  free(arr);
   return 0;
 }
