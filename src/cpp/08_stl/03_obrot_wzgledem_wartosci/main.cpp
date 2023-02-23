bc0db7db4303cca0523a450c2843132c1ed66edc
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-23 14:07:24 +0100

    [feature]: dodaj cpp/08_stl/ 03_obrot_wzgledem_wartosci/

---
+++ src/cpp/08_stl/03_obrot_wzgledem_wartosci/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void obroc(std::vector<int>& wektor, int klucz) {
+  // Wyszukiwanie pierwszego wystąpienia klucza
+    auto it = std::find(wektor.begin(), wektor.end(), klucz);
+    if (it != wektor.end()) {
+        // Obrót elementów przed kluczem na koniec wektora
+        std::rotate(wektor.begin(), it, wektor.end());
+    }
+}
+
+int main() {
+    // Tworzenie wektora losowych liczb całkowitych
+    std::vector<int> wektor {3, 2, 1, 7, 9, 7, 8, 10};
+
+    obroc(wektor, 9);
+
+    // Wyświetlenie wyniku
+    for (int x : wektor) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
