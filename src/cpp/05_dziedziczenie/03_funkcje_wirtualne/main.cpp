4ca76e9a3fcd9defb24ffe21dee68ce14a2e3b2b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-02-23 14:15:53 +0100

    [refactor]: popraw cpp/05_dziedziczenie/03_funkcje_wirtualne

--- src/cpp/05_dziedziczenie/03_funkcje_wirtualne/main.cpp
+++ src/cpp/05_dziedziczenie/03_funkcje_wirtualne/main.cpp
@@ -1,73 +1,26 @@
 #include <iostream>
 
-using namespace std;
-
 /*
-When derived class overrides the base class method by redefining the same
-function, then if client wants to access redefined the method from derived class
-through a pointer from base class object, then you must define this function in
-base class as virtual function.
+Kiedy klasa pochodna nadpisuje metodę klasy bazowej poprzez zdefiniowanie tej samej funkcji,
+to jeśli klient chce uzyskać dostęp do przedefiniowanej metody z klasy pochodnej
+przez wskaźnik z obiektu klasy bazowej, to należy zdefiniować tę funkcję w klasie bazowej jako funkcję wirtualną.
 */
 
-class parent {
+class Rodzic {
 public:
-  void Show() { cout << "i'm parent" << endl; }
-  virtual void v_Show() { cout << "i'm parent" << endl; }
+  void wypisz() { std::cout << "jestem rodzicem" << std::endl; }
+  virtual void wypiszWirtualnie() { std::cout << "jestem rodzicem" << std::endl; }
 };
 
-class child : public parent {
+class Dziecko : public Rodzic {
 public:
-  void Show() { cout << "i'm child" << endl; }
-  void v_Show() { cout << "i'm child" << endl; }
+  void wypisz() { std::cout << "jestem dzieckiem" << std::endl; }
+  void wypiszWirtualnie() { std::cout << "jestem dzieckiem" << std::endl; }
 };
 
 int main(int argc, char *argv[]) {
-  parent *parent_object_ptr = new child;
-  parent_object_ptr->Show();
-  parent_object_ptr->v_Show();
+  Rodzic *wskaznik = new Dziecko;
+  wskaznik->wypisz();
+  wskaznik->wypiszWirtualnie();
   return 0;
 }
-
-/*
-
-class Base
-{
-public:
-    virtual void print() const = 0;
-    virtual void printthat() const = 0;
-    virtual void printit() const = 0;
-};
-
-class inhert : public Base
-{
-public:
-    // only virtual keyword for overriding.
-    virtual void print() const {}
-
-    // only override keyword for overriding.
-    void printthat() const override {}
-
-    // using both virtual and override keywords for overriding.
-    virtual void printit() const override {}
-};
-
-When you override a function you don't technically need to write either virtual
-or override.
-
-The original base class declaration needs the keyword virtual to mark it as
-virtual.
-
-In the derived class the function is virtual by way of having the ¹same type as
-the base class function.
-
-However, an override can help avoid bugs by producing a compilation error when
-the intended override isn't technically an override. For instance, the function
-type isn't exactly like the base class function. Or that a maintenance of the
-base class changes that function's type, e.g. adding a defaulted argument.
-
-If some member function vf is declared as virtual in a class Base, and some
-class Derived, which is derived, directly or indirectly, from Base, has a
-declaration for member function with the same name parameter type list (but not
-the return type) cv-qualifiers ref-qualifiers Then this function in the class
-Derived is also virtual
-*/
